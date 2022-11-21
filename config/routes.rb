32f453295e87fea93b216430790e099755c3d99c
patriz gonzalez <patrizgonzalez@gmail.com>
2022-11-21 15:05:10 +0100

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :restaurants
   # Defines the root path route ("/")
   # root "articles#index"
+  # Failure/Error: require File.expand_path("../../config/environment", __FILE__)
+  # NameError:
+  # uninitialized constant SimpleForm
+
+  # SimpleForm.setup do |config|
+  # ^^^^^^^^^^
+  # Did you mean?  SimpleDelegator
 end
